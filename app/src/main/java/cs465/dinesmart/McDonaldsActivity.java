020244138f364f0478b1919898ab7c9133dd1633
Bob King <kingbob2015@hotmail.com>
2015-11-23 13:32:26 -0600

    McDonalds Restaurant Page

--- app/src/main/java/cs465/dinesmart/McDonaldsActivity.java
+++ app/src/main/java/cs465/dinesmart/McDonaldsActivity.java
@@ -3,11 +3,14 @@ package cs465.dinesmart;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class McDonaldsActivity extends AppCompatActivity {
+public class McDonaldsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +18,47 @@ public class McDonaldsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_mc_donalds);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle("McDonald's");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        String [] breakfastFood = {"Egg McMuffin","Hotcakes","Sausage Biscuit","Fruit 'N Yogurt Parfait"};
+        String [] burgerFood = {"Big Mac", "Quarter Pounder", "Hamburger","BBQ Ranch Burger"};
+        String [] chickenfishFood = {"McChicken","Chicken McNuggets","Premium McWrap Chicken and Ranch","Filet-O-Fish"};
+        String [] saladFood = {"Premium Bacon Ranch Salad","Premium Southwest Salad","Side Salad"};
+
+
+        ArrayAdapter<String> breakFastAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                breakfastFood
+        );
+        ListView breakfastList = (ListView) findViewById(R.id.listView);
+        breakfastList.setAdapter(breakFastAdapter);
+
+        ArrayAdapter<String> burgerAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                burgerFood
+        );
+        ListView burgerList = (ListView) findViewById(R.id.listView2);
+        burgerList.setAdapter(burgerAdapter);
+
+
+        ArrayAdapter<String> chickenfishAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                chickenfishFood
+        );
+        ListView chickenfishList = (ListView) findViewById(R.id.listView3);
+        chickenfishList.setAdapter(chickenfishAdapter);
+
+        ArrayAdapter<String> saladAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                saladFood
+        );
+        ListView saladList = (ListView) findViewById(R.id.listView4);
+        saladList.setAdapter(saladAdapter);
+
     }
 
 }
